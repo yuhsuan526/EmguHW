bbe1d40ae64bf1a1bdf27abcfd2c9d518bee04bb
YH <t104598001@ntut.edu.tw>
2015-11-12 15:13:19 +0800

    First commit

---
+++ emgu_project/emgu_project/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;           //for emguCV
+using Emgu.CV.Structure; //for Bgr
+
+namespace emgu_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filename = new OpenFileDialog();
+            if (filename.ShowDialog() == DialogResult.OK)
+            {
+                string name = filename.FileName;
+                Image img = new Bitmap(name);
+                pictureBox1.Image = img;
+            }
+
+        }
+    }
+}
